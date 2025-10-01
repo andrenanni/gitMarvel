83e4dc0ad4095435de8a18815a8b7526c1335960
andrenanni <andre-nanni@hotmail.com>
2025-10-01 18:50:35 -0300

    ajuste no codigo

--- src/api/characters.js
+++ src/api/characters.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import crypto from "crypto";
 
 export default async function handler(req, res) {
-    const name = req.query.name;
+    const name = req.query.name || "Spider-Man";
     
     const publicKey = process.env.MARVEL_PUBLIC_KEY;
     const privateKey = process.env.MARVEL_PRIVATE_KEY;
